16ece018cd669da389c8788e62b9df0545dec455
youna <dbsk0108@gmail.com>
2024-12-04 21:06:53 +0900

    ch9. Streaming

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,9 @@
+// export default function Loading() {
+//   return <div>Loading...</div>;
+// }
+
+import DashboardSkeleton from '../../ui/skeletons';
+
+export default function Loading() {
+  return <DashboardSkeleton />;
+}
