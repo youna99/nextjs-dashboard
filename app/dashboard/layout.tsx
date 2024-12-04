b27310c3e78a4b47a4979c013a48d94a91cd752f
youna <dbsk0108@gmail.com>
2024-12-04 22:35:05 +0900

    ch10. Partial Prerendering

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import SideNav from '../ui/dashboard/sidenav';
 
+// 아직 실험 단계 (partial prerendering)
+// export const experimental_ppr = true;
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // experimental: {
+  //   ppr: 'incremental',
+  // },
+  // 아직 실험단계로 최신에서만 사용 가능
 };
 
 export default nextConfig;
